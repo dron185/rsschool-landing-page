29f088a0caa31913d2f5f33440e98d6aa92c2785
Andrei Huryn <gurin.nord@gmail.com>
2026-09-21 20:55:13 +0300

    feat: add theme toggle

---
+++ script.js
@@ -0,0 +1,14 @@
+const themeToggle = document.querySelector('.header__theme-toggle');
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark');
+}
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    const theme = document.body.classList.contains('dark') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+});
