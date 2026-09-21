a7977a6f685ef70e52ed9cc04c0f43f731b9d692
Andrei Huryn <gurin.nord@gmail.com>
2026-09-21 23:28:28 +0300

    feat: add some styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const themeToggle = document.querySelector('.header__theme-toggle');
+// const logoIcon = document.querySelector('.logo-icon');
 
 const savedTheme = localStorage.getItem('theme');
 
@@ -8,6 +9,12 @@ if (savedTheme === 'dark') {
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark');
+
+    // if (document.body.classList.contains('dark')) {
+    //     logoIcon.src = '../assets/svg/logo-dark.svg';
+    // } else {
+    //     logoIcon.src = '../assets/svg/logo.svg';
+    // }
 
     const theme = document.body.classList.contains('dark') ? 'dark' : 'light';
     localStorage.setItem('theme', theme);
