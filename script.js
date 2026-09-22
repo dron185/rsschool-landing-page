41256106c948d5002fbc919e0ca17795a7cad1bf
Andrei Huryn <gurin.nord@gmail.com>
2026-09-22 16:08:10 +0300

    chore: replace logo.svg with use

--- script.js
+++ script.js
@@ -10,12 +10,6 @@ if (savedTheme === 'dark') {
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 
-    // if (document.body.classList.contains('dark')) {
-    //     logoIcon.src = '../assets/svg/logo-dark.svg';
-    // } else {
-    //     logoIcon.src = '../assets/svg/logo.svg';
-    // }
-
     const theme = document.body.classList.contains('dark') ? 'dark' : 'light';
     localStorage.setItem('theme', theme);
 });
